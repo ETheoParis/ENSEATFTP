7ce4f02d2392f2eb677c9ea5e5d06c61b13c47c8
ETheoParis <e.theo.paris@gmail.com>
2023-12-11 10:50:07 +0100

    fixed q2 (removed unneeded for loop)

--- src/gettftp.c
+++ src/gettftp.c
@@ -21,21 +21,21 @@ int main(int argc, char* argv[]){
 	struct addrinfo hints;
 	memset(&hints,0,sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
+	hints.ai_protocol = IPPROTO_UDP; 
 	
 	struct addrinfo* res;
 	
 	getaddrinfo(host, NULL, &hints, &res);
 	
-	struct addrinfo* rp;
 	
-	for(rp=res; rp!= NULL; rp = rp->ai_next){
-		void* addr;
-		char ipstr[INET6_ADDRSTRLEN];
-		struct sockaddr_in* ip = (struct sockaddr_in*) rp->ai_addr;
+	void* addr;
+	char ipstr[INET6_ADDRSTRLEN];
+	struct sockaddr_in* ip = (struct sockaddr_in*) res->ai_addr;
 		
-		addr = &(ip->sin_addr);
-		if(inet_ntop(rp->ai_family,addr,ipstr,sizeof(ipstr))==NULL){perror("wrong host name");}
-		
-		printf("ip : %s \n",ipstr);
-	}
+	addr = &(ip->sin_addr);
+	inet_ntop(res->ai_family,addr,ipstr,sizeof(ipstr));
+	printf("ip : %s \n",ipstr);
+	
+	//socket_sd = socket(rp->ai_family,rp->ai_socktype,);
+	
 }
